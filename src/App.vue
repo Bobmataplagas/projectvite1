7a7bb4110d27ab82472677aa26abb7a685895363
Bobmataplagas <alex.burgos2506@gmail.com>
2026-08-23 15:19:47 -0600

    cambios y añadidos en el contenido de los parrafos, diseño de colores más consistentes

--- src/App.vue
+++ src/App.vue
@@ -19,25 +19,46 @@ const movies=["Lord of the Rings", "Star Wars", "Dune", "Batman"]
     <h3 class="myCondition title">Alejandro Burgos Rodríguez</h3>
   </div>
 
-  <div style="border: solid blue;">
+  <div>
   <h2 class="name">Perfil</h2>
   <p class="highlight">Estudiante de quinto semestre de LATI en la UADY, nivel alto de inglés para escuchar, leer y nivel intermedio para hablar.</p>
   <p class="highlight">Tengo dominio en la computadora, gusto por las tecnologías, amor a los videojuegos, organizado y creativo  </p>
+  <p> _ </p>
   </div>
 
-  <div style="border: solid purple;">
+  <div class="border1">
   <h2 class="name">Educación</h2>
+    <p class = "color4">Carrera</p>
+    <p class="highlight">UADY Facultad de Contaduría </p>
+    <p class="highlight">Licenciatura en Administración Tecnologías de la información | Quinto semestre </p>
+    <p> _ </p>
+    <p class = "color4">Bachillerato</p>
+    <p class="highlight">UADY Preparatoria n°2 </p> 
+    <p class="highlight">Septiembre 2021 - Junio 2024 </p>
+    <p> _ </p>
+  </div>
+
+  <div class="border1">
   <ul>
-    <li>Metaphor: Re Fantazio</li>
-    <li>The World Ends With You</li>
-    <li>Persona 3</li>
-    <li>Golden Sun</li>
+    <h2 class = "name">Habilidades</h2>
+    <li><p class="highlight">Programación en Java</p></li>
+    <li><p class="highlight">Mantenimiento de equipos</p></li> 
+    <li><p class="highlight">Redes y comunicaciones</p></li> 
+    <li><p class="highlight">Organizado</p></li> 
+    <li><p class="highlight">Trabajo en equipo</p></li> 
+    <li><p class="highlight">Paquetería de Office (Word, Excel, PowerPoint)</p></li> 
+    <li><p class="highlight">Diseño de interfaz y experiencia de usuario</p></li> 
+    <p> _ </p>
   </ul>
   </div>
-
-  <div style="border: solid blue;">
-    <h2 class="name">Link de un video de mi canal:</h2>
-    <a href="https://youtu.be/Dg3izx-_44s">Video</a>
+  
+  <div class="border1">
+  <h2 class="name">Idiomas</h2>
+    <p class="color4">Inglés: </p>
+    <p class="highlight">Nivel alto para escuchar, leer y nivel intermedio para hablar</p>
+    <p> _ </p>
+    <p class="color4">Español: </p>
+    <p class="highlight">Nivel nativo</p>
   </div>
   
   <HelloWorld />
@@ -45,11 +66,11 @@ const movies=["Lord of the Rings", "Star Wars", "Dune", "Batman"]
 <style scoped>
 
 .border1{
-  border: solid rgba(255, 34, 34, 0.61);
+  border: solid rgba(252, 6, 6, 0.295);
 }
 
 .title{
-  color:rgb(216, 57, 57);
+  color:rgb(211, 57, 216);
 }
 
 .highlight{
@@ -59,5 +80,10 @@ const movies=["Lord of the Rings", "Star Wars", "Dune", "Batman"]
 .name{
   color:rgb(214, 34, 88);
 }
+
+.color4{
+  color:rgb(228, 28, 201);
+}
+
 </style>
 
